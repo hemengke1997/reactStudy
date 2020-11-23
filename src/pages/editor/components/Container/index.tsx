e0a13073eccf40da7ae1314c4d18901f0a2d4597
贺梦柯 <hemengke@newrank.cn>
2020-11-23 18:05:05 +0800

    正在完善编辑器页面

--- src/pages/editor/components/Container/index.tsx
+++ src/pages/editor/components/Container/index.tsx
@@ -1,11 +1,37 @@
 import React from 'react'
 
+import {
+  Tabs
+} from 'antd'
+
 import HeaderComponent from '../Header/index'
+import styles from './index.less'
+
+const { TabPane } = Tabs
 
 const ContainerComponent = () => {
   return (
-    <div>
+    <div className={styles.containerWrapper}>
       <HeaderComponent />
+
+
+      <div className={styles.container}>
+        {/* 左侧 */}
+        <div className={styles.leftArea}>
+
+          <div className={styles.componentList}>
+            <Tabs tabPosition="left">
+
+            </Tabs>
+          </div>
+          
+          <div className={styles.collapsed}>
+
+          </div>
+        </div>
+        {/* 右侧 */}
+        <div className={styles.rightArea}></div>
+      </div>
     </div>
   )
 }
--- src/pages/editor/index.tsx
+++ src/pages/editor/index.tsx
@@ -1,12 +1,18 @@
 import React from 'react'
 
-import HeaderComponent from './components/Header/index'
+// react-dnd
+import { DndProvider } from 'react-dnd'
+import { HTML5Backend } from 'react-dnd-html5-backend'
 
-const Editor = () => {
+import ContainerComponent from './components/Container/index'
+import styles from './index.less'
 
+const Editor = (props) => {
   return (
-    <div>
-      <HeaderComponent />
+    <div className={styles.layout}>
+      <DndProvider backend={HTML5Backend}>
+        <ContainerComponent {...props} />
+      </DndProvider>
     </div>
   )
 }
